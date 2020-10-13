5c3b5419075d0d080c7eb6952fb221a168a04c64
Jerome <nathan_9_9@hotmail.com>
2020-10-13 16:32:03 -0400

    test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float jumpForce = 100f;
+
+    private Rigidbody2D rbody;
+    private bool isGrounded = false;
+    private Animator anim;
+    private bool isFacingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+        //jump code 
+
+        if (isGrounded && Input.GetAxis("Jump")> 0)
+        {
+            rbody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+            
+        rbody.velocity = new Vector2(horiz * speed, rbody.velocity.y);
+
+        //check if the sprite needs to be fliped
+        if ((isFacingRight && rbody.velocity.x < 0) || (!isFacingRight && rbody.velocity.x > 0))
+        {
+            Flip();
+        }
+
+        // com with animator
+
+        anim.SetFloat("xVelocity", Mathf.Abs(rbody.velocity.x));
+        anim.SetFloat("yVelocity", rbody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+
+
+    }
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+        
+    }
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
+    }
+}
